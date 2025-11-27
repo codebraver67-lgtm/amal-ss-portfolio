d6adef35a376506112b8a8ea97011e26eb7224ca
codebraver67-lgtm <codebraver67@codebrave.org>
2025-11-27 12:00:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function () {
+document.getElementById("preloader").style.display = "none";
+});
+
+document.getElementById("contactform").addEventListener('submit',function(){
+    alert("thanks for contact!");
+})
+document.getElementById("back2").addEventListener("click",function(){
+    document.getElementById("back2").style.height="500px";
+})
